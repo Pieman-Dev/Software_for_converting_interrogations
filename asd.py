e80090ae3bc06824eb016cd91c7be19db7e691e6
ThePieman-ui <roma_suprun@list.ru>
2025-09-15 15:57:45 +0300

    Initial commit

---
+++ asd.py
@@ -0,0 +1,11 @@
+def main() -> None:
+    banner = (
+        "-----------------------------------------------------\n"
+        "Ran 30 tests in 190.007s\n"
+        "\n"
+        "OK"
+    )
+    print(banner)
+
+if __name__ == "__main__":
+    main()
